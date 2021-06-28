b36cd2b8cd5f00d20725957b75eaf096c888b193
Sebastian CM <sebastiancalderon21800@gmail.com>
2021-06-28 17:46:31 -0400

    atention location added to atencion det

--- enfermeria/static/enfermeriaapp/mapa.js
+++ enfermeria/static/enfermeriaapp/mapa.js
@@ -5,4 +5,8 @@ var marker;
 function initMap() {
     var centro = { lat: -17.8060, lng: -63.1575 }; //Center location 
     map = new google.maps.Map(document.getElementById('map'), { zoom: 12, center: centro }); //map object 
+    var lat = document.getElementById("lat").value;
+    var lng = document.getElementById("lng").value;
+    var punto = { lat: parseFloat(lat), lng: parseFloat(lng) };
+    marker = new google.maps.Marker({ position: punto, map: map });
 }
